d7520eabf4491fdec10eec810a16daa6305eab22
jstevenson2293 <jstevenson2293@gmail.com>
2023-04-20 22:12:37 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-//required abilities of a calculator:
-//accept user inputs of number, operator, and another number
-//should accept decimal numbers
-//store inputs
-//recognize inputs and perform calculations
-//return a result
+//required abilities of a calculator: -done
+//accept user inputs of number, operator, and another number -done
+//should accept decimal numbers -done
+//store inputs -done
+//recognize inputs and perform calculations -done
+//return a result -done
 
 //optional features:
 //Should accept longer arithemitc operations
@@ -29,44 +29,64 @@ const calculator = {
     prevTotal: null,
 
     parseInput(value) {
-        if (this.displayText === '0'){
-            this.displayText = ''
-        }
         //have any of the special buttons been clicked
         switch (value) {
-            case "=": 
+            case "=" : 
                 //calculate the answer
+                this.calcAnswer(this.displayText)
                 break;
             case "AC" :
                 //clear screen and stored values
+                this.clearAll()
                 break;
             case '.' :
                 if (this.displayText == 0) {
                     //pass '0' into add text method
-                }else {
+                    this.addText('0.')
+                } else {
                     //add value to text string
+                    this.addText(value)
                 } 
                 break;
             default:
                 //add value to text string
+                this.addText(value)
                 break;
         }
 
     },
 
-        addText(value) {
-            if(this.displayText === '0') {
-                this.displayText = ''
-            }else if(this.prevTotal !== null){
-                this.displayText = this.prevTotal
-                this.prevTotal = null
+    addText(value) {
+        if(this.displayText === '0') {
+            this.displayText = ''
+        }else if(this.prevTotal !== null){
+            this.displayText = this.prevTotal
+            this.prevTotal = null
+        }
+        if(isNaN(+(value)) && isNaN(+(this.displayText))) {
+            if(isNaN(this.displayText.slice(-1))){
+                return;
             }
-            if(/*user has entered an invalid sequence dont proceed*/){
-
-            }
-            this.displayText += value
-            //output display text to screen
+        }
+        this.displayText += value
+        //output display text to screen
+        this.outputText(this.displayText)
 
         },
+
+    outputText(text) {
+        document.querySelector('.calculator-screen').value = text
+    },
+
+    calcAnswer(equation) {
+        let result = Function("return " + equation)();
+        this.outputText(result);
+    },
+
+    clearAll() {
+        this.displayText = '0',
+        this.prevTotal = null,
+        this.outputText(this.displayText)
+    }
 }
 
